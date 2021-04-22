73a244ce21b867e08ab1c270ecdd57fa370adc4f
Oleksandr Lysun <ol@interlogic.com.ua>
2021-04-22 23:47:32 +0300

    add settings const

---
+++ app/src/main/java/com/oleksandrlysun/securemessenger/api/Settings.kt
@@ -0,0 +1,8 @@
+package com.oleksandrlysun.securemessenger.api
+
+object Settings {
+    const val SERVER_DATE_FORMAT = "yyyy-MM-dd"
+    const val APP_DATE_FORMAT = "dd/MM/yyyy"
+    const val SERVER_TIME_FORMAT = "HH:mm:ss"
+    const val APP_TIME_FORMAT = "HH:mm"
+}
